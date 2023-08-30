987e27c798826caa4cbf1ae5433e137ab24ef0d1
DavidOlawale <olawaledavid11@gmail.com>
2023-08-30 01:37:31 +0100

    add unit test project

---
+++ WatchPortalFunction.Tests/WatchFunctionUnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace WatchPortalFunction.Tests
+{
+    public class WatchFunctionUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
